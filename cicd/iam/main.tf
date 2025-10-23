6e08a83f675e0a2eefec2c86500d023e7b4bddd0
hszoo <trre1827151@gmail.com>
2025-10-23 11:59:32 +0900

    [Deploy] setup staging-based Terraform & AWS CICD Pipelines

--- cicd/iam/main.tf
+++ cicd/iam/main.tf
@@ -1,9 +1,41 @@
-resource "aws_ecr_repository" "cicd_ecr" {
-  name = "cicd-ecr"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-  tags = {
-    Name = "cicd-ecr"
-  }
+# CodeDeploy 역할
+resource "aws_iam_role" "codedeploy_role" {
+  name = "CodeDeployServiceRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "codedeploy.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+# EC2 인스턴스 역할
+resource "aws_iam_role" "ec2_role" {
+  name = "EC2CodeDeployRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+# EC2 인스턴스 프로파일
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "EC2CodeDeployInstanceProfile"
+  role = aws_iam_role.ec2_role.name
 }

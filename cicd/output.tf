1960065d412b9771f92164eeec36e04de4d5b0d7
hszoo <trre1827151@gmail.com>
2025-10-24 01:12:17 +0900

    ..

--- cicd/output.tf
+++ cicd/output.tf
@@ -1,3 +1,13 @@
 output "ec2_instance_profile" {
   value = module.iam.ec2_instance_profile
 }
+
+output "env" {
+  description = "Current environment name (e.g., dev, prod)"
+  value       = var.env
+}
+
+output "suffix" {
+  description = "Random unique suffix for resource names"
+  value       = random_id.suffix.hex
+}

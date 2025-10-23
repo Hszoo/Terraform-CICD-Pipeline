8b28a26d331f3913f2893425ec8708c9aaef1d36
hszoo <trre1827151@gmail.com>
2025-10-24 01:55:10 +0900

    ss

--- cicd/output.tf
+++ cicd/output.tf
@@ -1,7 +1,9 @@
+# IAM
 output "ec2_instance_profile" {
   value = module.iam.ec2_instance_profile
 }
 
+# 환경 정보
 output "env" {
   description = "Current environment name (e.g., dev, prod)"
   value       = var.env
@@ -10,4 +12,27 @@ output "env" {
 output "suffix" {
   description = "Random unique suffix for resource names"
   value       = random_id.suffix.hex
+}
+
+# ECR
+output "ecr_repository_name" {
+  description = "ECR repository name"
+  value       = module.ecr.repository_name
+}
+
+# S3
+output "s3_bucket_name" {
+  description = "S3 artifact bucket name"
+  value       = module.s3.bucket_name
+}
+
+# CodeDeploy
+output "codedeploy_app_name" {
+  description = "CodeDeploy Application name"
+  value       = module.codedeploy.app_name
+}
+
+output "codedeploy_group_name" {
+  description = "CodeDeploy Deployment Group name"
+  value       = module.codedeploy.group_name
 }

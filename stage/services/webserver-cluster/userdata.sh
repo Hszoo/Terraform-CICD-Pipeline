9757a6e5acda80b404388b80c3e8f43a8c55a404
hszoo <trre1827151@gmail.com>
2025-10-23 14:05:00 +0900

    [Fix] change EC2 instance user

--- stage/services/webserver-cluster/userdata.sh
+++ stage/services/webserver-cluster/userdata.sh
@@ -2,7 +2,7 @@
 yum update -y
 yum install -y ruby wget
 
-cd /home/ec2-user
+cd /home/ubuntu
 wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
